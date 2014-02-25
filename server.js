bb8a95a96e270edac896c4bd7a249690ae589d76
E L <ekluo1@gmail.com>
2014-02-25 01:47:03 -0600

    migrating from codelab 7

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
-var WebSocketServer = require('ws').Server
-  , http = require('http')
-  , express = require('express')
-  , app = express()
-  , port = process.env.PORT || 5000;
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(2014);
 
-app.use(express.static(__dirname + '/'));
 
-var server = http.createServer(app);
-server.listen(port);
+// var express = require('express');
+// var app = express();
+// console.log(express.static(__dirname + '/js'));
+// app.use(express.static(__dirname + '/js'));
+// app.all('*', function(req, res){
+// 	res.sendfile("index.html");
+// });
 
-console.log('http server listening on %d', port);
+// app.listen(9000);
 
-var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
 
-    console.log('websocket connection open');
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
 
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
-    });
+	function log(){
+		var array = [">>> "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+
+	socket.on('message', function (message) {
+		log('Got message: ', message);
+		socket.broadcast.emit('message', message); // should be room only
+	});
+
+	socket.on('create or join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room', room);
+
+		if (numClients == 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients == 1) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+	});
+
 });
+

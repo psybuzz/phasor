e86f97b0951b0f8af7dd82ffa8b164c10481fe58
E L <ekluo1@gmail.com>
2014-02-25 01:52:45 -0600

    port env variable for heroku fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(2014);
+}).listen(port = process.env.PORT || 2014);
 
 
 // var express = require('express');
